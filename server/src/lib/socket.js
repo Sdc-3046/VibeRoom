24297977aa4fbb661cbe64264f6f4eb0b58e4764
Shubham Chavan <chavanshubham879@gmail.com>
2024-12-28 11:23:45 +0000

    Removed unused imports from socket.js

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -1,8 +1,6 @@
 import { Server } from 'socket.io'
 import http from 'http'
 import express from 'express'
-import { Socket } from 'dgram'
-import { disconnect } from 'process'
 
 const app = express()
 const server = http.createServer(app)
